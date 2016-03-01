70a20cb7a1aeaba46d582aeea051fa3f2ee5d323
Adir Amsalem <adira@wix.com>
2016-03-01 15:12:33 +0200

    remove tasks folder

--- tasks/css.js
+++
@@ -1,8 +0,0 @@
-var gulp = require('gulp');
-var sass = require('gulp-sass');
-
-gulp.task('css', function () {
-    gulp.src('src/**/*.scss')
-        .pipe(sass().on('error', sass.logError))
-        .pipe(gulp.dest('src'));
-});
--- tasks/serve.js
+++
@@ -1,9 +0,0 @@
-var gulp = require('gulp');
-var nodemon = require('gulp-nodemon');
-
-gulp.task('serve', function () {
-    nodemon({
-        script: 'serve.js',
-        ext: 'js css html'
-    });
-});
