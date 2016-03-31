89d7bfe52b02b971dd25e7de0a95f928d97dde88
lotemh <lotemh@wix.com>
2016-03-31 17:53:18 +0300

    #SE-12385| use appTabs

--- src/settings/settings.js
+++ src/settings/settings.js
@@ -1,33 +1,27 @@
-define(['react', 'editor-ui-lib', './pages/pages'], function (React, UI, pages) {
-    return React.createClass({
-        settingsUpdate: function (key, value) {
-            const data = {key: key, value: value};
-            Wix.Settings.triggerSettingsUpdatedEvent(data);
-        },
-        render: function () {
-            const verticalTabsStyle = {height: '1000px'};
-            return (
-                <UI.verticalTabs defaultTab="tab0" style={verticalTabsStyle}>
-                    <UI.tabs.header>
-                        {pages.map((page, pageIndex) => {
+var UI = require('editor-ui-lib');
+require('react');
+var pages = require('./pages/pages');
+
+var settings =  React.createClass({
+    settingsUpdate: function (key, value) {
+        const data = {key: key, value: value};
+        Wix.Settings.triggerSettingsUpdatedEvent(data);
+    },
+    render: function () {
+        return (
+            <div>
+                <UI.appSettings>
+                    <UI.appTabs defaultTabIndex={0} onChange={function(index){console.log('tab was changed to: ' + index);}}>
+                        {pages.map((page) => {
                             return (
-                                <UI.tabs.label for={'tab' + pageIndex} key={pageIndex} className="layout">
-                                    {page.label}
-                                </UI.tabs.label>
+                                <page.content tab={page.label} onUpdate={(key, value)=>this.settingsUpdate(key, value)}/>
                             );
                         })}
-                    </UI.tabs.header>
-                    <UI.tabs.content>
-                        {pages.map((page, pageIndex) => {
-                            return (
-                                <UI.tabs.tab key={pageIndex} name={'tab' + pageIndex} className="my-custom-tab">
-                                    <page.content onUpdate={(key, value)=>this.settingsUpdate(key, value)}/>
-                                </UI.tabs.tab>
-                            );
-                        })}
-                    </UI.tabs.content>
-                </UI.verticalTabs>
-            )
-        }
-    });
+                    </UI.appTabs>
+                </UI.appSettings>
+            </div>
+        );
+    }
 });
+
+module.exports = settings;
