9dc6851da87c011d45b468358f88780db6a0dc9f
Lior Shefer <lior.shefer@wix.com>
2016-08-29 08:35:17 +0300

    updating sample app enabling hot reload and fixing some elements that didn't work

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,7 @@ var webpack = require('webpack');
 
 var getPlugins = function() {
     var plugins = [];
+    var hotReload = new webpack.HotModuleReplacementPlugin()
     var noErrorsPlugin = new webpack.NoErrorsPlugin();
     var uglifyPlugin = new webpack.optimize.UglifyJsPlugin({
         compressor: {
@@ -11,8 +12,10 @@ var getPlugins = function() {
     });
 
     plugins.push(noErrorsPlugin);
-    if (process.argv.indexOf('prd') > -1) {
+    if (process.argv.indexOf('prod') > -1) {
         plugins.push(uglifyPlugin)
+    } else {
+        plugins.push(hotReload)
     }
 
     return plugins;
@@ -20,27 +23,31 @@ var getPlugins = function() {
 
 var webpackConfig = {
     entry: {
-        settings: __dirname + '/src/settings/settings_main',
+        settings: [
+            'webpack-dev-server/client?http://localhost:3000',
+            'webpack/hot/only-dev-server',
+            __dirname + '/src/settings/App'
+            ],
         widget: __dirname + '/src/widget/widget_main'
     },
     output: {
         path: path.join(__dirname, 'dist'),
+        sourceMapFilename: "debugging/[file].map",
         filename: '[name].js'
     },
     plugins: getPlugins(),
     externals: {
         react: 'React',
-        'react/addons': 'React',
         'react-dom': 'ReactDOM',
         lodash: '_',
         jquery: '$',
-        angular: 'angular',
         Wix: 'Wix',
         'editor-ui-lib': 'UI'
     },
+    devtool: 'eval',
     module: {
         loaders: [
-            {test: /\.js?$/, loader: 'babel', include: path.join(__dirname, 'src')},
+            {test: /\.js?$/, loaders: ['babel-loader?presets[]=es2015&presets[]=react', 'react-hot'], include: path.join(__dirname, 'src')},
             {test: /\.js$/, exclude: [/node_modules/], loader: 'babel-loader'},
             {test: /\.rt/, loader: "react-templates-loader"},
             {test: /\.css$/, loader: "style-loader!css-loader"},
