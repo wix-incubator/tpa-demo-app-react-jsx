d7d329aeac498561b39aafc5ce739563645b64f1
Adiel <adiela@wix.com>
2016-08-31 13:53:08 +0300

    fixing widget

--- src/widget/widget.js
+++ src/widget/widget.js
@@ -11,12 +11,12 @@ define(['react', 'Wix'], function (React, Wix) {
 
             // You can get the style params programmatically, un-comment the following snippet to see how it works:
             /*Wix.Styles.getStyleParams(function (style) {
-                console.log(style);
+             console.log(style);
              });*/
 
             // You can also get the style every time it changes, try this:
             /*Wix.addEventListener(Wix.Events.STYLE_PARAMS_CHANGE, function (style) {
-                console.log(style);
+             console.log(style);
              });*/
         },
         onSettingsUpdate: function (update) {
@@ -39,20 +39,19 @@ define(['react', 'Wix'], function (React, Wix) {
             const {settingsUpdate} = this.state;
             return (
                 <div>
-
-                <div class="wix-style-sample">
-                     <h3 class="sample-element sample-title">Demo App</h3>
-                <p class="sample-element sample-content">Welcome to the Wix Demo App, let's play!</p>
-                <form class="form">
-                        <input title="email" type="email" class="sample-element sample-input" value="Enter text here"/>
-                </form>
-                <button class="sample-element sample-button">Click me!</button>
-                <br/>
-                      <h3 class="sample-element sample-title" >Last settings update</h3>
-                <pre>
-                     <code class="json sample-content">{{settingsUpdate}}</code>
-                </pre>
-            </div>
+                    <div className="wix-style-sample">
+                        <h3 className="sample-element sample-title">Demo App</h3>
+                        <p className="sample-element sample-content">Welcome to the Wix Demo App, let's play!</p>
+                        <form className="form">
+                            <input title="email" type="email" className="sample-element sample-input" value="Enter text here"/>
+                        </form>
+                        <button className="sample-element sample-button">Click me!</button>
+                        <br/>
+                        <h3 className="sample-element sample-title">Last settings update</h3>
+                        <pre>
+                             <code className="json sample-content">{{settingsUpdate}}</code>
+                        </pre>
+                    </div>
                 </div>
             )
         }
