e86e65af37eaf72d8cee448b0a678dedb23d47df
Adiel <adiela@wix.com>
2016-12-06 14:21:51 +0200

    save for weekly

--- src/settings/modules/main/main.js
+++ src/settings/modules/main/main.js
@@ -22,65 +22,16 @@ export default class Main extends React.Component {
     render () {
         return (
             <div className="main-tab">
-                <UI.sectionDividerLabeled
-                    label="Animation settings"
-                    infoText="info text"
-                    infoTitle="info title"
-                />
-
                 <UI.checkbox
-                    value="{this.state.checkboxValue}"
-                    label="Checkbox"
-                    value={this.state.booleanCheckbox}
-                    disabled={false}
-                    labelAfterSymbol={false}
-                    onChange={(newVal)=>this.setState({booleanCheckbox: newVal})}
-                />
-
-                <UI.checkbox
-                    value={this.state.booleanCheckbox}
+                    wix-param="checkboxValue"
                     label="checkbox with labelAfterSymbol"
-                    defaultValue={false}
-                    disabled={true}
                     labelAfterSymbol={true}
                 />
 
-                <UI.radioButtons
-                    title="Select position"
-                    disabled={true}
-                    infoTitle="blabla"
-                    infoText="klakla"
-                    options={[
-                        { value: '1', label: 'first', disabled: true },
-                        { value: '2', label: 'second' },
-                        { value: '3', label: 'third' }
-                    ]}
-                    value={this.state.dropDownSelectedValue}
-                    onChange={(newVal)=>this.setState({dropDownSelectedValue: newVal})}
-                />
-
-                <UI.button className="btn-confirm-primary" onClick={()=>this.onClick()}>
-                    Main CTA
-                </UI.button>
-
-
-                <span>dropDownSelect is throwing an error</span>
-                <UI.dropDownSelect
-                    value={this.state.dropDownSelectedValue}
-                    onChange={(newVal)=>this.setState({dropDownSelectedValue: newVal})}
-                    options={[
-                        {value: '1', label: 'main'},
-                        {value: '2', label: 'sub level 1'},
-                        {value: '3', label: 'sub 2 level 1'}
-                    ]}
-                />
-
-                <UI.textInput
-                    value={this.state.inputValue}
-                    label="lay bell"
-                    type="password"
-                    placeholder="placeholder"
-                    onChange={this.onInputValueChange}
+                <UI.checkbox
+                    wix-param="checkboxValue"
+                    label="checkbox with labelAfterSymbol"
+                    labelAfterSymbol={true}
                 />
 
             </div>
--- src/settings/settings.js
+++ src/settings/settings.js
@@ -3,6 +3,11 @@ import React from 'react'
 import Wix from 'Wix';
 import Main from './modules/main/main';
 
+const DBInitialState = {
+    checkboxValue: false,
+    falseyValue: false
+}
+
 export default class settings extends React.Component {
     settingsUpdate (key, value) {
         const data = {key: key, value: value};
@@ -12,9 +17,11 @@ export default class settings extends React.Component {
 
     render () {
         return (
-            <UI.panelTabs defaultTabIndex={0}>
-                <Main tab="Main"/>
-            </UI.panelTabs>
+            <UI.appSettings DBClass={UI.styleParamDB} DBInitialState={DBInitialState}>
+                <UI.panelTabs defaultTabIndex={0}>
+                    <Main tab="Main"/>
+                </UI.panelTabs>
+            </UI.appSettings>
         )
     }
 }
