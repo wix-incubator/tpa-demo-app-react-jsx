10310e434ede4de5bf3410bb22f1fed7ecb91518
Adiel <adiela@wix.com>
2016-12-12 12:42:30 +0200

    save

--- src/settings/modules/main/main.js
+++ src/settings/modules/main/main.js
@@ -5,15 +5,28 @@ export default class Main extends React.Component {
     render () {
         return (
             <div className="main-tab">
-                <UI.checkbox
-                    wix-param="booleanValue"
-                    label="checkbox with labelAfterSymbol"
-                    labelAfterSymbol={true}
-                />
+                <UI.button>
+                    button label
+                </UI.button>
+
+                <UI.sectionDividerLabeled>section Divider Labeled</UI.sectionDividerLabeled>
+
+                <UI.divider long={true} />
+                <br/>
+                <UI.divider />
+
+                <UI.textInput />
+                <UI.textInputMultiline />
 
                 <UI.toggleSwitch
                     wix-param="booleanValue"
                     label="checkbox with labelAfterSymbol"
+                />
+
+                <UI.checkbox
+                    wix-param="booleanValue"
+                    label="checkbox with labelAfterSymbol"
+                    labelAfterSymbol={true}
                 />
 
                 <UI.radioButtons
@@ -37,8 +50,34 @@ export default class Main extends React.Component {
                 />
 
                 <h1 style={{    backgroundColor: 'rgb(220, 217, 217)', margin: 0, padding: '4px 10px', border: '1px solid #c2c2f5', fontSize: '24px', borderWidth: '1px 0'}}>old school components</h1>
+
+                <UI.tooltip value="tooltip">
+                    <span style={{background: 'yellow', padding: '5px', margin: '5px'}}>hover me! I have a lovely tooltip!</span>
+                </UI.tooltip>
+
+                <UI.toggleButtons
+                    wix-param="numeric1-3Value"
+                    options={[
+                        {value: 1, label: 'main'},
+                        {value: 2, label: 'sub level 1'},
+                        {value: 3, label: 'sub 2 level 1'}
+                    ]}
+                />
+
+                <UI.toggleButtonsGroup
+                    options={[
+                        {value: 1, label: 'main'},
+                        {value: 2, label: 'sub level 1'},
+                        {value: 3, label: 'sub 2 level 1'}
+                    ]}
+                />
+
                 <UI.fontPicker title_title="Font Picker" infoIcon_title="title" infoIcon_text="text" wix-param="fontPickerParam" />
-                <UI.colorPickerInput title="Color Picker" wix-param="colorPickerParam" />
+                <UI.fontAndColorPicker title_title="Font Picker" infoIcon_title="title" infoIcon_text="text" wix-param="fontPickerParam" />
+                <UI.colorPickerInput title="Color Picker" wix-param="colorPickerParam"/>
+                <UI.colorPickerSlider title="Color Picker" wix-param="colorPickerParam"/>
+
+                <UI.teaserPopup text="teaser popup text" title="teaser popup title"/>
             </div>
         )
     }
