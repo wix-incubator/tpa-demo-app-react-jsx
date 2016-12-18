c88cc43afd0e6c38fa1e82301afc8222d9269cb5
Adiel <adiela@wix.com>
2016-12-18 17:50:33 +0200

    using available wix-base-ui components.

--- src/settings/settings.js
+++ src/settings/settings.js
@@ -2,12 +2,26 @@ import UI from 'editor-ui-lib';
 import React from 'react'
 import Wix from 'Wix';
 import Main from './modules/main/main';
+import Settings from './modules/settings/settings';
+import Layout from './modules/layout/layout';
+import Design from './modules/design/design';
+import Animations from './modules/animations/animations';
+import Support from './modules/support/support';
 
 const defaultDBValue = {
-    booleanValue: false,
-    'numeric1-3Value': '2',
-    fontPickerParam: {},
-    colorPickerParam: {}
+    /* Settings tab */
+    whenImageIsClickedDD: '1',
+    allowEditingSwitch: true,
+    allowDeletingSwitch: true,
+    settingsCheckboxName: false,
+    settingsCheckboxTitle: true,
+    settingsCheckboxDescription: false,
+    settings_textOnImages: '2',
+
+    /* Animations tab */
+    animationType: '1',
+    animations_direction: '2',
+    animations_oppositeEffect: true
 }
 
 export default class settings extends React.Component {
@@ -22,13 +36,28 @@ export default class settings extends React.Component {
             <UI.appSettings DBClass={UI.styleParamDB} defaultDBValue={defaultDBValue}>
                 <UI.panelTabs defaultTabIndex={0}>
                     <UI.panelTab label="Main">
-                        <Main/>
+                        <Main onUpdate={this.settingsUpdate}/>
                     </UI.panelTab>
-                    <UI.panelTab label="MainDuplicate">
-                        <Main/>
+                    <UI.panelTab label="Settings">
+                        <Settings onUpdate={this.settingsUpdate}/>
                     </UI.panelTab>
+                    <UI.panelTab label="Layout">
+                        <Layout onUpdate={this.settingsUpdate}/>
+                    </UI.panelTab>
+                    <UI.panelTab label="Design">
+                        <Design onUpdate={this.settingsUpdate}/>
+                    </UI.panelTab>
+                    <UI.panelTab label="Animations">
+                        <Animations onUpdate={this.settingsUpdate}/>
+                    </UI.panelTab>
+                    <UI.panelTab label="Support">
+                        <Support onUpdate={this.settingsUpdate}/>
+                    </UI.panelTab>
+                    <UI.button className="btn-upgrade-nav">Upgrade</UI.button>
                 </UI.panelTabs>
             </UI.appSettings>
         )
     }
 }
+/*
+*/
