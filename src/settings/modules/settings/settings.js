fd22b41fbe142a590d9268c792488518566279d7
Adiel <adiela@wix.com>
2016-12-19 15:17:00 +0200

    with composites

--- src/settings/modules/settings/settings.js
+++ src/settings/modules/settings/settings.js
@@ -5,96 +5,106 @@ export default class Settings extends React.Component {
     render () {
         return (
             <div>
-                <ul className="comments">
-                    <li>title is missing</li>
-                </ul>
-                <UI.dropDownSelect
-                    wix-param="whenImageIsClickedDD"
-                    onChange={(newVal)=>this.props.onUpdate('settings_onImageClick', newVal)}
-                    options={[
-                        { value: '1', label: 'Enter expand mode'},
-                        { value: '2', label: 'Show on full screen'},
-                        { value: '3', label: 'Do nothing'}
-                    ]}
-                />
+                <UI.Composites.DropDownLabeled>
+                    <UI.textLabel value="When image is clicked" shouldTranslate={false}/>
+                    <UI.dropDownSelect
+                        wix-param="whenImageIsClickedDD"
+                        onChange={(newVal)=>this.props.onUpdate('settings_onImageClick', newVal)}
+                        options={[
+                            { value: '1', label: 'Enter expand mode'},
+                            { value: '2', label: 'Show on full screen'},
+                            { value: '3', label: 'Do nothing'}
+                        ]}
+                    />
+                </UI.Composites.DropDownLabeled>
 
                 <UI.divider long={true} />
                 <UI.sectionDividerLabeled>Enable functionality</UI.sectionDividerLabeled>
                 <UI.divider long={true} />
 
-                <UI.toggleSwitch
-                    wix-param="allowEditingSwitch"
-                    label="Allow editing"
-                    onChange={(newVal)=>this.props.onUpdate('settings_allowEditing', newVal)}
-                />
+                <UI.Composites.ToggleSwitchUnlabeled>
+                    <UI.toggleSwitch
+                        wix-param="allowEditingSwitch"
+                        label="Allow editing"
+                        onChange={(newVal)=>this.props.onUpdate('settings_allowEditing', newVal)}
+                    />
+                </UI.Composites.ToggleSwitchUnlabeled>
 
                 <UI.divider long={true} />
 
-                <UI.toggleSwitch
-                    wix-param="allowDeletingSwitch"
-                    label="Allow editing"
-                    onChange={(newVal)=>this.props.onUpdate('allowDeletingSwitch', newVal)}
-                />
+                <UI.Composites.ToggleSwitchUnlabeled>
+                    <UI.toggleSwitch
+                        wix-param="allowDeletingSwitch"
+                        label="Allow editing"
+                        onChange={(newVal)=>this.props.onUpdate('allowDeletingSwitch', newVal)}
+                    />
+                </UI.Composites.ToggleSwitchUnlabeled>
 
                 <UI.divider long={true} />
                 <UI.sectionDividerLabeled>Display info</UI.sectionDividerLabeled>
                 <UI.divider long={true} />
 
-                <UI.checkbox
-                    wix-param="settingsCheckboxName"
-                    label="Name"
-                    labelAfterSymbol={true}
-                    onChange={(newVal)=>this.props.onUpdate('settingsCheckboxName', newVal)}
-                />
 
-                <UI.checkbox
-                    wix-param="settingsCheckboxTitle"
-                    label="Title"
-                    labelAfterSymbol={true}
-                    onChange={(newVal)=>this.props.onUpdate('settingsCheckboxTitle', newVal)}
-                />
+                <UI.Composites.CheckboxesUnlabeled>
+                    <UI.checkbox
+                        wix-param="settingsCheckboxName"
+                        label="Name"
+                        labelAfterSymbol={true}
+                        onChange={(newVal)=>this.props.onUpdate('settingsCheckboxName', newVal)}
+                    />
 
-                <UI.checkbox
-                    wix-param="settingsCheckboxDescription"
-                    label="Description"
-                    labelAfterSymbol={true}
-                    onChange={(newVal)=>this.props.onUpdate('settingsCheckboxDescription', newVal)}
-                />
+                    <UI.checkbox
+                        wix-param="settingsCheckboxTitle"
+                        label="Title"
+                        labelAfterSymbol={true}
+                        onChange={(newVal)=>this.props.onUpdate('settingsCheckboxTitle', newVal)}
+                    />
 
-                <UI.divider long={true} />
-
-                <UI.radioButtons
-                    title="Do images show text?"
-                    wix-param="settings_textOnImages"
-                    options={[
-                            { value: '1', label: 'A title and a description'},
-                            { value: '2', label: 'Just a title'},
-                            { value: '3', label: 'Just a description'}
-                        ]}
-                    onChange={(newVal)=>this.props.onUpdate('settings_textOnImages', newVal)}
-                />
+                    <UI.checkbox
+                        wix-param="settingsCheckboxDescription"
+                        label="Description"
+                        labelAfterSymbol={true}
+                        onChange={(newVal)=>this.props.onUpdate('settingsCheckboxDescription', newVal)}
+                    />
+                </UI.Composites.CheckboxesUnlabeled>
 
                 <UI.divider long={true} />
 
-                <ul className="comments">
-                    <li>title is missing, use composite</li>
-                </ul>
-                <UI.textInput
-                    defaultText="Title"
-                    onChange={(newVal)=>this.props.onUpdate('settings_textFieldContent', newVal)}
-                />
+
+                <UI.Composites.RadioButtonsLabeled>
+                    <UI.textLabel value="Do images show text?" shouldTranslate={false}/>
+                    <UI.radioButtons
+                        wix-param="settings_textOnImages"
+                        options={[
+                                { value: '1', label: 'A title and a description'},
+                                { value: '2', label: 'Just a title'},
+                                { value: '3', label: 'Just a description'}
+                            ]}
+                        onChange={(newVal)=>this.props.onUpdate('settings_textOnImages', newVal)}
+                    />
+                </UI.Composites.RadioButtonsLabeled>
 
                 <UI.divider long={true} />
 
-                <ul className="comments">
-                    <li>title is missing, use composite</li>
-                </ul>
-                <UI.textInputMultiline
-                    title="Text area"
-                    focus={true}
-                    defaultText="Paragraph"
-                    onChange={(newVal)=>this.props.onUpdate('settings_textAreaContent', newVal)}
-                />
+
+                <UI.Composites.TextInputLabeled>
+                    <UI.textLabel value="Text field" shouldTranslate={false}/>
+                    <UI.textInput
+                        defaultText="Title"
+                        onChange={(newVal)=>this.props.onUpdate('settings_textFieldContent', newVal)}
+                    />
+                </UI.Composites.TextInputLabeled>
+
+                <UI.divider long={true} />
+
+                <UI.Composites.TextInputLabeled>
+                    <UI.textLabel value="Text area" shouldTranslate={false}/>
+                    <UI.textInputMultiline
+                        focus={true}
+                        defaultText="Paragraph"
+                        onChange={(newVal)=>this.props.onUpdate('settings_textAreaContent', newVal)}
+                    />
+                </UI.Composites.TextInputLabeled>
             </div>
         )
     }
