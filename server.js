4c66046888cbb3675c50a449fa704cfd4e3e6067
Billy Kovalsky <billy@yotpo.com>
2018-09-03 17:48:41 +0300

    add https:true to the wepback server config

--- server.js
+++ server.js
@@ -5,7 +5,8 @@ var config = require('./webpack.config');
 new WebpackDevServer(webpack(config), {
     publicPath: config.output.publicPath,
     hot: true,
-    historyApiFallback: true
+    historyApiFallback: true,
+    https: true,
 }).listen(3000, 'localhost', function (err, result) {
     if (err) {
         return console.log(err);
