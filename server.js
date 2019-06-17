08eac3fdbda44f5f51a5653d5384c3de35d7ff16
Uri <Uricorn@users.noreply.github.com>
2019-06-17 17:50:15 +0300

    Update console log message to reflect HTTPS

--- server.js
+++ server.js
@@ -12,5 +12,5 @@ new WebpackDevServer(webpack(config), {
         return console.log(err);
     }
 
-    console.log('Listening at http://localhost:3000/');
+    console.log('Listening at https://localhost:3000/');
 });
